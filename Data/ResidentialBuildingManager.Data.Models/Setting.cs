a9ee7f24a6e1cb00d1e971c34b83991c8839596e
Blagoy Dzhiganski <skaarj_warrior@hotmail.com>
2020-03-22 22:02:37 +0200

    Initial Commit

---
+++ Data/ResidentialBuildingManager.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace ResidentialBuildingManager.Data.Models
+{
+    using ResidentialBuildingManager.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Web/ResidentialBuildingManager.Web/Controllers/BaseController.cs
@@ -0,0 +1,8 @@
+﻿namespace ResidentialBuildingManager.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BaseController : Controller
+    {
+    }
+}
